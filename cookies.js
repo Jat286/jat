f9266a340580fce3ceeb0ebfc3e2ca3729e334a6
Jat286 <73901875+Jat286@users.noreply.github.com>
2020-12-14 15:51:00 +0000

    c3

--- cookies.js
+++ cookies.js
@@ -2,3 +2,7 @@ var x = document.cookie;
 document.cookie = "username=username";
 document.cookie = "password=*********";
 document.cookie = "dateofbirth=ddmmyyyy";
+            function othername() {
+                var input = document.getElementById("userInput").value;
+                document.cookie = input;
+            }
